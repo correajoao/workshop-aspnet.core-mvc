1387f6c3284bcb1c1ee9031421022b8588f3aab7
correajoao <johnac.victor@gmail.com>
2021-07-07 16:52:17 -0300

    Foreign key not null (Referential integrity)

--- SelectWebMvc/Migrations/20210707192802_DepartmentForeignKey.Designer.cs
+++ SelectWebMvc/Migrations/20210707192802_DepartmentForeignKey.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SelectWebMvc.Models;
 
 namespace SelectWebMvc.Migrations
 {
     [DbContext(typeof(SelectWebMvcContext))]
-    partial class SelectWebMvcContextModelSnapshot : ModelSnapshot
+    [Migration("20210707192802_DepartmentForeignKey")]
+    partial class DepartmentForeignKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
